4551a59aed7cbf982dc66d59c86819a17c1ac437
alden <63831735+aldenasf@users.noreply.github.com>
2025-07-04 21:53:03 +0700

    feat: implement Converter component and update layout structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    output: "server", // Enable SSR
 
-  integrations: [react(), icon()]
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [react(), icon()],
 });
